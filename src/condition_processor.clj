67c7d952421558ec564d92adab1bbf2f74f3574f
luciana <lpiazzi@despegar.com>
2018-04-03 22:33:49 -0300

    operation evaluator

---
+++ src/condition_processor.clj
@@ -0,0 +1,27 @@
+(ns condition-processor (:use [clojure.string :as string]))
+
+(defn apply-operation [identifier args]
+      (cond
+        (= identifier '=) (= (first args) (second args))
+        (= identifier 'and) (and (first args) (second args))
+        (= identifier 'or) (or (first args) (second args))
+        (= identifier '!=) (not= (first args) (second args))
+        (= identifier 'includes?) (string/includes? (first args) (second args))))
+
+(defn current? [value] (= (nth value 0) 'current ))
+(defn past? [value] (= (nth value 0) 'past ))
+
+(defn pass? [condition data past-data]
+      "Given a current data and a past data, determines if the given condition is true"
+      (cond
+        (current? condition) (get data (nth condition 1))
+        (past? condition) (get past-data (nth condition 1))
+        :else
+        (apply-operation (first condition) (map #(pass? % data past-data) (rest condition)))))
+
+;; return true if the condition is met
+;; data is the current data being processed
+;; past-data is collection of data previously processed
+;; see the manual for the correctness of the condition expression
+(defn pass-condition? [condition data past-data]
+      (first (filter #(pass? condition data %) past-data)))
---
+++ test/condition_processor_test.clj
@@ -0,0 +1,33 @@
+(ns condition-processor-test
+  (:require [clojure.test :refer :all]
+            [condition-processor :refer :all]))
+
+;;todo completar tests.
+
+(def condition1 '(and (!= (current "sender") (past "receiver")) (includes? (past "subject") (current "subject"))))
+(def condition2 '(or (= (current "sender") (past "receiver")) (includes? (past "subject") (current "subject"))))
+(def condition3 '(and (= (current "sender") (past "receiver")) (includes? (past "subject") (current "subject"))))
+(def condition4 '(and (= (current "sender") (past "receiver")) (includes? (past "subject") (current "subject"))))
+
+(def data1 {"sender" "ramona" "receiver" "jorge" "subject" "holo"})
+(def data2 {"sender" "culkin" "receiver" "ramona" "subject" "holo"})
+(def data3 {"sender" "ramona" "receiver" "scott" "subject" "carta importante"})
+(def data4 {"sender" "ramona" "receiver" "scott" "subject" "holo"})
+
+(def dataActual {"sender" "ramona" "receiver" "scott" "subject" "hola como va"})
+
+(def past-data [data1 data2 data3])
+
+(deftest pass-condition-test
+  (testing "Should pass condition given data"
+    (is (= false
+           (pass? condition1 data1 data2)))
+    (is (= true
+           (pass? condition1 data1 data4)))
+    (is (= false
+            (pass? condition1 data1 data2)))
+    (is (= false
+            (pass? condition1 data1 data2)))))
+
+
+
