461d4c8954712a6815206443d5463c742f4ab4d4
florencia95 <florenciaerramuspe@hotmail.com>
2018-04-10 15:28:43 -0300

    fix get-state at state-initializer and get-counter at query-processor

--- src/query_processor.clj
+++ src/query_processor.clj
@@ -1,13 +1,12 @@
 (ns query-processor)
 
 (defn get-counter [name state]
-  (filter #(= (% :name) name) state)
-)
+  (filter #(= (% :name) name)  (:rules state))
+  )
 
 (defn equal-args? [keyTT args]
   (= (compare keyTT args) 0)
-)
+  )
 
 (defn get-value [row params]
-  (:value (first (filter #(equal-args? (% :key) params) (:truth-table (first row)))))
-)
+  (:value (first (filter #(equal-args? (% :key) params) (:truth-table (first row))))))
