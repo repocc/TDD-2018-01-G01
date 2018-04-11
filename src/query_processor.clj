5bd5f8f96869fbca7a1786e8283316118a8bbb0b
Luciana P <lucianasl@gmail.com>
2018-04-10 21:21:30 -0300

    fix in query-processor

--- src/query_processor.clj
+++ src/query_processor.clj
@@ -5,11 +5,11 @@
   )
 
 (defn equal-args? [keyTT args]
-  (= (compare keyTT args) 0)
-  )
+  (= (compare keyTT args) 0))
 
 (defn maybe-get-value [row params]
-  (:value (first (filter #(equal-args? (% :key) params) (:truth-table (first row))))))
+  (do (print row))
+  (:value (first (filter #(equal-args? (:key %) params) (:truth-table (first row))))))
 
 (defn get-value [row params]
   (if (nil? (maybe-get-value row params)) 0 (maybe-get-value row params)))
