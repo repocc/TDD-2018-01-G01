6d1c6728115db77a9ed494779345e60786ba9716
Franco Mazzoni <franco.mazzoni73@gmail.com>
2018-04-10 21:57:53 -0300

    Merge

--- src/data_processor.clj
+++ src/data_processor.clj
@@ -1,20 +1,22 @@
-(ns data-processor (:use [state-initializer :refer :all]))
+(ns data-processor (:use [state-initializer :refer :all]
+                         [counter-increaser :refer :all]
+                         [query-processor :refer :all]))
 
 (defn initialize-processor [rules]
   (get-state rules))
 
 (defn process-data [state new-data]
-      (let [new-state (process-new-state state new-data)])
-  [(new-state) (process-signals new-state data past-data)])
+      ;;(let [new-state (process-counter state new-data)]))
+  [(process-counter state new-data) []])
+  ;new-state) (process-signals new-state data past-data)])
 
 (defn query-counter [state counter-name counter-args]
-	(get-value (get-counter counter-name state) counter-args)  
+	(get-value (get-counter counter-name state) counter-args)
 )
 
 
-(defn process-new-state [old-state new-data]
-      (do (def rules (increment-counters old-state new-data))
-          (def past-data (save-past-data old-state new data))
-          ({:rules rules :past-data past-data})))
-
-(defn process-signals [state data past-data] ([]))
+;;(defn process-new-state [old-state new-data]
+;;      (do (def rules (process-counter old-state new-data))
+;;          (def past-data (save-past-data old-state new data))
+;;
+;;(defn process-signals [state data past-data] ([]))
