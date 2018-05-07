c9ae304fa6e1627e8575f5ea39c8c166beb6edc4
Franco Mazzoni <franco.mazzoni73@gmail.com>
2018-05-07 19:11:00 -0300

    Creating Maven project with some dependencies and a template of jetty server

---
+++ tp/src/main/java/tdd/tp/Main.java
@@ -0,0 +1,28 @@
+package main.java.tdd.tp;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ComponentScan
+public class Main implements WebMvcConfigurer {
+
+    public static void main(String args[]) throws Exception {
+
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(Main.class);
+
+        Server server = new Server(8080);
+        ServletContextHandler servletContextHandler = new ServletContextHandler();
+        ServletHolder servletHolder = new ServletHolder(new DispatcherServlet(webContext));
+        servletContextHandler.addServlet(servletHolder, "/*");
+
+        server.setHandler(servletContextHandler);
+        server.start();
+        server.join();
+    }
+}
