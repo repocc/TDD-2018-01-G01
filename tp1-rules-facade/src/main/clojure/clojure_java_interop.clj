d921817140e472890c63e28192c1c2d8c8bde4ca
luciana <lpiazzi@despegar.com>
2018-06-03 17:47:22 -0300

    fixed in rules when parsing json to symbol in operations

--- tp1-rules-facade/src/main/clojure/clojure_java_interop.clj
+++ tp1-rules-facade/src/main/clojure/clojure_java_interop.clj
@@ -3,21 +3,17 @@
                                    [clojure.walk :as walk])
                          (:gen-class))
 
-(def rules '((define-counter "email-count" []
-                             true)
-              (define-signal {"spam-fraction" (/ (counter-value "spam-count" [])
-                                                 (counter-value "email-count" []))}
-                             true)
+(def rules '(
+              (define-counter "string-test" [] (= (current "name") "John"))
               ))
 
 (defn -initialize []
   (json/write-str (initialize-processor rules)))
 
 (defn -process-data [state new-data]
-  (do (println (json/read-str state))
+  (do (println (json/read-str new-data))
       (json/write-str (first (process-data (walk/keywordize-keys (json/read-str state)) (json/read-str new-data))) ) )
   )
 
 (defn -add-rule [state new-rules]
   (json/write-str (add-rule (walk/keywordize-keys (json/read-str state)) (json/read-str new-rules) )))
-
--- tp1-rules-facade/src/main/java/app/MainClass.java
+++ tp1-rules-facade/src/main/java/app/MainClass.java
@@ -7,18 +7,18 @@ public class MainClass {
         RulesValidatorApp rules = RulesValidatorApp.getInstance();
         String state = rules.getState();
 
+        //String rule1 = "[[\"define-counter\",\"spam-count\",[],[\"current\",\"spam\"]]]";
+        //String rule2 = "[[\"define-signal\",{\"spam-fraction\":[\"\\/\",[\"counter-value\",\"spam-count\",[]],[\"counter-value\",\"email-count\",[]]]},true]]";
+
+        //rules.addRules(rule1);
+
         String data0 = "{\"name\":\"John\",\"age\":30,\"sender\":true}";
         String data1 = "{\"name\":\"John\",\"age\":30,\"important\":true}";
         String data2 = "{\"name\":\"John\",\"age\":30,\"spam\":true}";
 
-        rules.processData(data0);
-        rules.processData(data1);
-        rules.processData(data2);
-
-        String rule1 = "[[\"define-counter\",\"spam-count\",[],[\"current\",\"spam\"]]]";
-        String rule2 = "[[\"define-signal\",{\"spam-fraction\":[\"\\/\",[\"counter-value\",\"spam-count\",[]],[\"counter-value\",\"email-count\",[]]]},true]]";
-
-        rules.addRules(rule1);
+        state = rules.processData(data0);
+        state = rules.processData(data1);
+        state = rules.processData(data2);
 
         System.out.println(state);
 
