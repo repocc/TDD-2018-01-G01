5b4c58a8a90104c07f48e5d6aacd942cee053f74
Franco Mazzoni <franco.mazzoni@despegar.com>
2018-06-10 20:22:56 -0300

    Add Buttons when project is created

--- ticket-system/src/sample/ProjectController.java
+++ ticket-system/src/sample/ProjectController.java
@@ -5,10 +5,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectController {
 
+    private static final Double SPACE_BETWEEN_PROJECTS = 10.0;
+
+    @FXML
+    private AnchorPane projectScreen;
     @FXML
     private Pane editionMenu;
     @FXML
@@ -23,7 +31,9 @@ public class ProjectController {
     private Button projectUsers;
     @FXML
     private Button projectFinished;
-
+    private Map<Integer,Project> projects = new HashMap<>();
+    private static int projectCount = 0;
+    private Double initialHeight = 115.0;
 
     public void createProject() {
         this.editionMenu.setStyle("-fx-background-color: white;");
@@ -33,5 +43,17 @@ public class ProjectController {
         this.states.setVisible(true);
         this.projectUsers.setVisible(true);
         this.projectFinished.setVisible(true);
+    }
+
+    public void addProject() {
+        Project project = new Project(projectName.getText());
+        projects.put((++projectCount),project);
+        Button projectButton = new Button();
+        projectButton.setText(projectName.getText());
+        projectButton.setPrefSize(300, 30);
+        projectButton.setLayoutX(58);
+        projectButton.setLayoutY(initialHeight + projectButton.getPrefHeight() + SPACE_BETWEEN_PROJECTS);
+        initialHeight = projectButton.getLayoutY();
+        projectScreen.getChildren().add(projectButton);
     }
 }
